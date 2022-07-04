6d3d9f6118ec02930521ec02cf3ee3ace705b80b
Robin Lenaerts <r.lenaerts@easi.net>
2022-07-04 17:18:22 +0200

    initial push

---
+++ AnabelTrivia/Data/DatabaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnabelTrivia.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Use Fluent API to configure  
+
+            modelBuilder.Entity<Question>(entity =>
+            {
+                entity.ToTable("Questions");
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.QuestionText).IsRequired();
+                entity.Property(e => e.Category).IsRequired();
+            });
+        }
+    }
+}
---
+++ AnabelTrivia/Data/Question.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnabelTrivia.Data
+{
+    public class Question
+    {
+        [Key] public int ID { get; set; }
+        [DataType(DataType.Text)] public string QuestionText { get; set; }
+        [DataType(DataType.Text)] public string Category { get; set; }
+    }
+}
