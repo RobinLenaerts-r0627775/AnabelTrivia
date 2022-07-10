a89e1919fa250e881e4cef7fd7d9f9d5c28a4d43
Robin Lenaerts <r.lenaerts@easi.net>
2022-07-10 23:39:42 +0200

    cleaning up code + making app functional

--- AnabelTrivia/Controller/HomeController.cs
+++ AnabelTrivia/Controller/HomeController.cs
@@ -11,156 +11,44 @@ namespace AnabelTrivia.Controller
 {
     public class HomeController : Microsoft.AspNetCore.Mvc.Controller
     {
-        private readonly DatabaseContext _context;
+        private readonly DatabaseContext db;
 
         public HomeController(DatabaseContext context)
         {
-            _context = context;
+            db = context;
         }
 
         // GET: Questions
         public async Task<IActionResult> IndexAsync()
         {
-            var questions = _context.Questions.ToList();
-            return View(questions);
-        }
-
-        // GET: Questions/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Questions == null)
-            {
-                return NotFound();
-            }
-
-            var question = await _context.Questions
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (question == null)
-            {
-                return NotFound();
-            }
-
-            return View(question);
-        }
-
-        // GET: Questions/Create
-        public IActionResult Create()
-        {
+            var categories = db.Questions.Where(x => x.Used == "N").Select(x => x.Category).Distinct().ToList();
+            ViewBag.Categories = categories;
             return View();
         }
 
-        // POST: Questions/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,QuestionText,Category")] Question question)
+        public IActionResult Random(string category)
         {
-            if (ModelState.IsValid)
+            var rand = new Random();
+
+            var toSkip = rand.Next(0, db.Questions.Where(x => x.Used != "Y" && x.Category == category).Count());
+            var random = db.Questions.Where(x => x.Used != "Y" && x.Category == category).Skip(toSkip).Take(1).First();
+            random.Used = "Y";
+            db.Questions.Update(random);
+            db.SaveChanges();
+
+            var counter = db.Questions.Where(x => x.Used == "N").Count();
+            if (counter == 0)
             {
-                _context.Add(question);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(IndexAsync));
-            }
-
-            return View(question);
-        }
-
-        // GET: Questions/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Questions == null)
-            {
-                return NotFound();
-            }
-
-            var question = await _context.Questions.FindAsync(id);
-            if (question == null)
-            {
-                return NotFound();
-            }
-
-            return View(question);
-        }
-
-        // POST: Questions/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,QuestionText,Category")] Question question)
-        {
-            if (id != question.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+                foreach (var record in db.Questions.ToList())
                 {
-                    _context.Update(question);
-                    await _context.SaveChangesAsync();
+                    record.Used = "N";
                 }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!QuestionExists(question.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-
-                return RedirectToAction(nameof(IndexAsync));
             }
 
-            return View(question);
-        }
-
-        // GET: Questions/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Questions == null)
-            {
-                return NotFound();
-            }
-
-            var question = await _context.Questions
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (question == null)
-            {
-                return NotFound();
-            }
-
-            return View(question);
-        }
-
-        // POST: Questions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Questions == null)
-            {
-                return Problem("Entity set 'DatabaseContext.Questions'  is null.");
-            }
-
-            var question = await _context.Questions.FindAsync(id);
-            if (question != null)
-            {
-                _context.Questions.Remove(question);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(IndexAsync));
-        }
-
-        private bool QuestionExists(int id)
-        {
-            return (_context.Questions?.Any(e => e.ID == id)).GetValueOrDefault();
+            db.SaveChanges();
+            var categories = db.Questions.Where(x => x.Used == "N").Select(x => x.Category).Distinct().ToList();
+            ViewBag.Categories = categories;
+            return View(random);
         }
     }
 }
